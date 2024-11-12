ffbd8d238eec4ba6b3fad481daea2dc24b5c8a09
Daniel Bachhuber <daniel.b@posthog.com>
2024-11-12 15:43:27 -0800

    Only back date the initial event by 12 hours

--- cli.js
+++ cli.js
@@ -59,7 +59,7 @@ program
 
       for (const event of defaultEvents) {
         const timestamp = new Date(
-          Date.now() - 24 * 60 * 60 * 1000
+          Date.now() - 12 * 60 * 60 * 1000
         ).toISOString();
         posthog.capture({
           event,
